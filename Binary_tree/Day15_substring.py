3be4049d25da4308e2001998f957f49b4e5c39a5
HiteshKhandelwal901 <hitesh.khandelwal_ug21@ashoka.edu.in>
2021-11-30 16:34:31 +0530

    substring_Added

---
+++ Binary_tree/Day15_substring.py
@@ -0,0 +1,26 @@
+def issub(s1,s2):
+    size1 = len(s1)
+    size2 = len(s2)
+    i = 0
+    j = 0
+    print("size1 = ", size1, " size2 = ", size2)
+    
+    while(j<size2):
+        print("i = ", i)
+        print("j = ", j)
+        if s1[i] == s2[j]:
+            print("inside if", s1[i], s2[j])
+            i = i+1
+            j = j+1
+
+        else:
+            i = 0
+            j = j+1
+
+        if i == size1:
+            return True
+
+    return False
+
+if __name__ == "__main__":
+    print(issub("lmlo", "helloworls"))
